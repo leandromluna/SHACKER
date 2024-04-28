5a2cfc32d919940df28bea63bed59b4ec98a4b5c
Leandro <leandro.luna25@hotmail.com>
2024-04-28 15:47:42 -0300

    Timeconversion java

---
+++ HackerRank/10.Time Conversion/TimeConversion.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+        int horas = Integer.parseInt(s.substring(0,2));
+        String minutosSegundos = s.substring(2, 8);
+        String meridiano = s.substring(8);
+        if(meridiano.equals("PM") && horas != 12){
+             horas += 12;
+        } else if(meridiano.equals("AM") && horas == 12){
+             horas = 0;
+        }   
+    return String.format("%02d%s", horas, minutosSegundos);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
