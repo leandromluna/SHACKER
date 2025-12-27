ab6cb916599dda28856e79a0e6ce5ccedb35b519
leanmluna <leandro.luna25@hotmail.com>
2025-12-27 12:05:23 -0300

    Comments Complexity, tradeoff Ex1

--- Leetcode-Medium/Container-WithMostWater/R.c
+++ Leetcode-Medium/Container-WithMostWater/R.c
@@ -1,18 +1,23 @@
 int maxArea(int* height, int heightSize) {
-    int i = 0;
-    int j = heightSize - 1;
-    int best = 0;
+    int i = 0; //O(1)
+    int j = heightSize - 1; //O(1)
+    int best = 0; //O(1)
 
-    while (i < j) {
-        int h = height[i] < height[j] ? height[i] : height[j];
-        int w = j - i;
-        int area = h * w;
+    while (i < j) { // O(n)
+        int h = height[i] < height[j] ? height[i] : height[j]; //O(1)
+        int w = j - i; //O(1)
+        int area = h * w; //O(1)
 
-        if (area > best) best = area;
+        if (area > best) best = area; //O(1)
 
-        if(height[i]  < height[j]) i++;
+        if(height[i]  < height[j]) i++; //O(1)
         else 
-           j--;
+           j--; //O(1)
     }
     return best;
 }
+// O(n * 1) = O(n)
+
+// Time Complexity: O(n)
+// The space complexity: O(1)
+// Trade-off: Lose width for height to maximize area, lose simplicity of brute-force for efficiency n velocity.
