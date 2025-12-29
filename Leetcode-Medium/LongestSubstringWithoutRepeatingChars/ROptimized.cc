2506fdbd01e69857024fb18bbd93e6734e635185
leanmluna <leandro.luna25@hotmail.com>
2025-12-29 09:11:25 -0300

    add Solution Optimized c++ Ex2

---
+++ Leetcode-Medium/LongestSubstringWithoutRepeatingChars/ROptimized.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> last(256, -1);   // última posición vista de cada char
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            if (last[s[right]] >= left)
+                left = last[s[right]] + 1;
+
+            last[s[right]] = right;
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};
+
+//O(n*1) 
+// Time Complexity: O(n)
+// The space complexity: O(1)
+// Trade-off: Faster because it jumps directly to the next valid position, but slightly harder to understand.
